26a8f67fd0b5b978e447a04a578b235e0d4ab0d4
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-04 15:38:51 +0200

    first

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,9 @@ def pascal_triangle(n):
     """Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
     """
-    if len(n) <= 0:
-        return []
-
     triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
     for i in range(n):
         line = []
         for j in range(i + 1):
